dd830295078fc76d56a5ebaa620afa0bfa374366
TafatsuM <turkdemirmustafa@gmail.com>
2021-07-29 21:13:45 +0300

    Initialize project using Create React App

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Button from 'react-bootstrap/Button';
+
+function Home() {
+    return (
+            <Container>
+  <Navbar expand="lg" variant="light" bg="light">
+    <Container>
+      <Navbar.Brand href="#">Navbar</Navbar.Brand>
+    </Container>
+  </Navbar>
+</Container>
+    )
+}
+
+export default Home
