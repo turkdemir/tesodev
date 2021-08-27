8388b025fa894e9b53097c352cfef2465275069c
TafatsuM <turkdemirmustafa@gmail.com>
2021-08-27 09:14:47 +0300

    ert

--- src/components/ContentList.js
+++ src/components/ContentList.js
@@ -1,38 +1,57 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 let mockData = require("../mockData.json");
 
-export class Content extends Component {
-  render() {
-    const filteredItems = this.props.filtered;
+function Content(props) {
+  const [state, setstate] = useState([]);
 
-    return (
-      <div className="container p-3" style={{ width: "700px", margin:"0 auto", }}>
-        <div className="align-self-end" style={{ position: "absolute", right:"15%"}}>
-        <i class="fas fa-exchange-alt rotate" style={{ position: "relative", left:"20px"}}></i>
-        <select className="px-4 custom-select" id="inputGroupSelect01" style={{ width: "180px", border:"none"}} onChange={this.props.sortBy}>
+  useEffect(() => {
+    setstate(props.filtered);
+  }, [props.filtered]);
+
+  return (
+    <div className="container p-3" style={{ width: "700px", margin: "0 auto" }}>
+      <div
+        className="align-self-end"
+        style={{ position: "absolute", right: "15%" }}
+      >
+        <i
+          className="fas fa-exchange-alt rotate"
+          style={{ position: "relative", left: "20px" }}
+        ></i>
+        <select
+          className="px-4 custom-select"
+          id="inputGroupSelect01"
+          style={{ width: "180px", border: "none" }}
+          onChange={props.sortBy}
+        >
           <option value="0">Order By</option>
           <option value="NameA">Name ascending</option>
           <option value="NameD">Name descending</option>
           <option value="YearA">Year ascending</option>
           <option value="YearD">Year descending</option>
         </select>
-        </div>
+      </div>
 
-        <table className="table table-hover table-sm" style={{ width: "100%", height:"600px"}}>
-          <thead>
-            <tr>
-              {mockData.cols.map((element, index) => {
-                return (
-                  <th scope="col" key={index}>
-                    {element}
-                  </th>
-                );
-              })}
-            </tr>
-          </thead>
-          <tbody>
-            {filteredItems && filteredItems.map((element, index) => {
+      <table
+        className="table table-hover table-sm"
+        style={{ width: "100%", height: "600px" }}
+      >
+        <thead>
+          <tr>
+            {mockData.cols.map((element, index) => {
+              return (
+                <th scope="col" key={index}>
+                  {element}
+                </th>
+              );
+            })}
+          </tr>
+        </thead>
+        <tbody>
+          {state &&
+            state.length > 0 &&
+            state.map((element, index) => {
               return (
                 <tr key={index}>
                   {element.map((item, index) => (
@@ -41,11 +60,10 @@ export class Content extends Component {
                 </tr>
               );
             })}
-          </tbody>
-        </table>
-      </div>
-    );
-  }
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default Content;
--- src/components/List.js
+++ src/components/List.js
@@ -21,7 +21,7 @@ function Home(props) {
     const filteredItems = mockData.data.filter((item) =>
       item.join(",").toLowerCase().includes(inputVal.toLowerCase())
     );
-
+    console.log(filteredItems)
     setfiltered(filteredItems);
   };
 
@@ -72,7 +72,7 @@ function Home(props) {
   };
   const arr = ["helll", "04/02/1979"];
   console.log(arr[1].split("/")[2]);
-  console.log(inputVal, sorted, pageOfItems);
+  console.log(inputVal, sorted, pageOfItems, filtered);
 
   return (
     <main className="container p-0 rounded" style={{ backgroundColor: "#FFF" }}>
